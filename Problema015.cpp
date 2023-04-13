94b7175c395abb33b3b0484daf17210c1254e91c
tontoriales <122110356+tontoriales@users.noreply.github.com>
2023-04-13 17:44:53 -0500

    Add files via upload

---
+++ Problema015.cpp
@@ -0,0 +1,15 @@
+//Problema015
+//Crear un algoritmo para calcular el interés compuesto generado
+//por un capital depositado durante cierta cantidad de tiempo a
+//una tasa de interés determinada, aplique las siguientes 
+//fórmulas: M=(1+(r/100))™*C  |  I=M-C
+#include <iostream>
+using namespace std;
+int main() {
+	int N, S;
+	cout<<"Numero: ";
+	cin>>N;
+	S = (N * (N + 1))/2;
+	cout<<"Suma: "<<S;
+	return 0;
+}
